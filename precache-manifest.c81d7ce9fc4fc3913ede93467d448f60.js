c7977dd1415a3f2d55ba2a976a7db9a8ce71294f
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-06 20:06:48 -0700

    Updates

---
+++ precache-manifest.c81d7ce9fc4fc3913ede93467d448f60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d9462122995cdf8e426dc73ed85e61",
+    "url": "/Photo-Shopper/index.html"
+  },
+  {
+    "revision": "d986a67321c17de4beb9",
+    "url": "/Photo-Shopper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03f1bcd37d4560cc8305",
+    "url": "/Photo-Shopper/static/js/2.7d4770a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Photo-Shopper/static/js/2.7d4770a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d986a67321c17de4beb9",
+    "url": "/Photo-Shopper/static/js/main.24f3cefa.chunk.js"
+  },
+  {
+    "revision": "c91cae521f9b8018c2c9",
+    "url": "/Photo-Shopper/static/js/runtime-main.d3f17aa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Photo-Shopper/static/media/logo.5d5d9eef.svg"
+  }
+]);
