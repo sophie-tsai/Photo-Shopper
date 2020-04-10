f18853158131a366a690efbf87d029ab7b8629e0
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-10 12:07:08 -0700

    Incorporated Animated Icons

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,10 +1,24 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useCallback } from "react";
 import Image from "../components/Image";
 import { getClass } from "../utils/grid";
 import { PicContext } from "../picContext";
+import lottie from "lottie-web";
 
 function Photos() {
   const { allPhotos } = useContext(PicContext);
+
+  const ref = useCallback((node) => {
+    if (node !== null) {
+      lottie.loadAnimation({
+        container: node,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path:
+          "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/iphone-spinner-1/iphone-spinner-1.json",
+      });
+    }
+  }, []);
 
   const photoElements = allPhotos.map((photo, index) => {
     return <Image key={photo.id} img={photo} className={getClass(index)} />;
@@ -14,7 +28,9 @@ function Photos() {
     <>
       <main className="photos">
         {allPhotos.length === 0 ? (
-          <h3 className="loading">Loading...</h3>
+          <div id="loadingDiv" ref={ref}>
+            <h3 className="loading">Loading...</h3>
+          </div>
         ) : (
           photoElements
         )}
