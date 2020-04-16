b5f8cc2ed1715a362952418174fb0b6b8e3cc5ee
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-16 12:10:33 -0700

    Added Search Bar UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,16 +10,37 @@ function Header() {
 
   return (
     <div className="header">
-      <Link to="/Photo-Shopper/">
-        <i className="ri-home-4-line ri-fw ri-2x"></i>
-      </Link>
+      <div className="headerContainer">
+        <div className="headerTextDiv">
+          <span className="headerText">PhotoShopper</span>
+        </div>
 
-      <div className="headerDiv">
-        <span className="headerText">Photo Shopper</span>
+        <div className="searchbar">
+          <input
+            type="text"
+            name="search"
+            placeholder="search beautiful images"
+          />
+        </div>
+
+        <div className="headerIcons">
+          <div className="home-icon">
+            <Link to="/Photo-Shopper/">
+              <i className="ri-home-4-line ri-fw ri-2x"></i>
+            </Link>
+          </div>
+
+          {/* <div className="heart-icon">
+            <i className="ri-heart-line ri-fw ri-2x"></i>
+          </div> */}
+
+          <div className="cart-icon">
+            <Link to="/Photo-Shopper/cart">
+              <i className={`${cartClassName} ri-fw ri-2x`}></i>
+            </Link>
+          </div>
+        </div>
       </div>
-      <Link to="/Photo-Shopper/cart">
-        <i className={`${cartClassName} ri-fw ri-2x`}></i>
-      </Link>
     </div>
   );
 }
